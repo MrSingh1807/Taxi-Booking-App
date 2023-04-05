b29a6a250f9adb6cbe53774df842300c9adc0c7e
Mayank Singh Shakya <mayank1807.singh@gmail.com>
2023-04-06 02:33:33 +0530

    phone number verification complete

--- app/src/main/java/com/example/taxibookingapp/UI/AuthenticateUser/PhoneLoginFragment.java
+++ app/src/main/java/com/example/taxibookingapp/UI/AuthenticateUser/PhoneLoginFragment.java
@@ -2,6 +2,7 @@ package com.example.taxibookingapp.UI.AuthenticateUser;
 
 import static android.app.Activity.RESULT_OK;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -9,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
@@ -17,15 +19,31 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.IntentSenderRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.airbnb.lottie.Lottie;
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieComposition;
+import com.example.taxibookingapp.UI.HomeActivity;
 import com.example.taxibookingapp.databinding.FragmentPhoneLoginBinding;
-import com.google.android.gms.auth.api.credentials.Credential;
-import com.google.android.gms.auth.api.credentials.CredentialsApi;
 import com.google.android.gms.auth.api.identity.GetPhoneNumberHintIntentRequest;
 import com.google.android.gms.auth.api.identity.Identity;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.hbb20.CountryCodePicker;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 
 public class PhoneLoginFragment extends Fragment {
@@ -34,6 +52,14 @@ public class PhoneLoginFragment extends Fragment {
     private String SELECTED_COUNTRY_CODE = "+91";
     private static final int CREDENTIAL_PICKER_REQUEST = 120;
     public static final String TAG = "MeriTaxi";
+
+    /* ************  Firebase Phone Auth  ************** */
+    private String mVerificationID;
+    private PhoneAuthProvider.ForceResendingToken mResentToken;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks phoneAuthVerificationStateChangeCallbacks;
+    private FirebaseAuth firebaseAuth;
+    /* ************  Firebase Phone Auth  ************** */
+
     ActivityResultLauncher<IntentSenderRequest> phoneNumberHintIntentResultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartIntentSenderForResult(), new ActivityResultCallback<ActivityResult>() {
                 @Override
@@ -43,7 +69,7 @@ public class PhoneLoginFragment extends Fragment {
                             String phoneNumber;
                             try {
                                 phoneNumber = Identity.getSignInClient(requireActivity()).getPhoneNumberFromIntent(result.getData());
-                                if (phoneNumber.contains("+91")){
+                                if (phoneNumber.contains("+91")) {
                                     phoneNumber = phoneNumber.replace("+91", "");
                                 }
                             } catch (ApiException e) {
@@ -60,6 +86,12 @@ public class PhoneLoginFragment extends Fragment {
                 }
             });
 
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -85,21 +117,8 @@ public class PhoneLoginFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
-                if (charSequence.length() == 10) {
-                    Toast.makeText(requireContext(), "OTP is sent to your Mobile Number ", Toast.LENGTH_SHORT).show();
-                    /*
-                    binding.phoneLoginCLayout.animate()
-                            .translationY(binding.phoneLoginCLayout.getHeight())
-                            .alpha(0.0f)
-                            .setDuration(500);
-                    binding.phoneLoginCLayout.setVisibility(View.GONE);
-                    binding.firstPinView.animate()
-                            .translationY(binding.firstPinView.getHeight())
-                            .alpha(1.0f)
-                            .setDuration(500);
-                    binding.firstPinView.setVisibility(View.VISIBLE);
-                     */
-                }
+                if (charSequence.length() == 10)
+                    sendOTP();
             }
 
             @Override
@@ -118,8 +137,13 @@ public class PhoneLoginFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
-                if (charSequence.length() == 6)
-                    Toast.makeText(requireContext(), "OTP Submitted", Toast.LENGTH_SHORT).show();
+                if (charSequence.length() == 6) {
+                    binding.progressBar.setVisibility(View.VISIBLE);
+
+                    PhoneAuthCredential authCredential =
+                            PhoneAuthProvider.getCredential(mVerificationID, Objects.requireNonNull(binding.firstPinView.getText()).toString());
+                    signInWithAuthCredential(authCredential);
+                }
             }
 
             @Override
@@ -129,21 +153,6 @@ public class PhoneLoginFragment extends Fragment {
         });
 
         /* ************   Auto Phone Select  ************** */
-        /*  HintRequest hintRequest = new HintRequest.Builder()
-                .setPhoneNumberIdentifierSupported(true)
-                .build();
-
-
-        PendingIntent intent = Credentials.getClient(requireContext()).getHintPickerIntent(hintRequest);
-        try
-        {
-            startIntentSenderForResult(intent.getIntentSender(), CREDENTIAL_PICKER_REQUEST, null, 0, 0, 0,new Bundle());
-        }
-        catch (IntentSender.SendIntentException e)
-        {
-            e.printStackTrace();
-        }
-         */
         GetPhoneNumberHintIntentRequest hintRequest = GetPhoneNumberHintIntentRequest.builder().build();
         Identity.getSignInClient(requireActivity())
                 .getPhoneNumberHintIntent(hintRequest)
@@ -156,8 +165,75 @@ public class PhoneLoginFragment extends Fragment {
                     Log.d(TAG, e.getMessage());
                 });
 
+        /* ************   OTP Callbacks  ************** */
+        phoneAuthVerificationStateChangeCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                String code = phoneAuthCredential.getSmsCode();
+                if (code != null) {
+                    binding.firstPinView.setText(code);
+                    signInWithAuthCredential(phoneAuthCredential);
+                }
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                Toast.makeText(requireContext(), "Something Went Wrong...", Toast.LENGTH_SHORT).show();
+
+                setVisibility(View.GONE, View.VISIBLE, View.GONE);
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+
+                mVerificationID = s;
+                mResentToken = forceResendingToken;
+
+                Toast.makeText(requireContext(), "6 Digit OTP sent", Toast.LENGTH_SHORT).show();
+                setVisibility(View.GONE, View.GONE, View.VISIBLE);
+            }
+        };
 
         return binding.getRoot();
+    }
+
+    private void signInWithAuthCredential(PhoneAuthCredential phoneAuthCredential) {
+        firebaseAuth.signInWithCredential(phoneAuthCredential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(requireContext(), "Logged In SuccessFully", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(requireActivity(), HomeActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(requireContext(), "LogIn Failed", Toast.LENGTH_SHORT).show();
+
+                            Navigation.findNavController(binding.contactNumberET).popBackStack();
+
+                        }
+                    }
+                });
+    }
+
+    private void sendOTP() {
+        binding.progressBar.setVisibility(View.VISIBLE);
+
+        String phoneNumber = SELECTED_COUNTRY_CODE + binding.contactNumberET.getText();
+        PhoneAuthOptions authOptions = PhoneAuthOptions.newBuilder(firebaseAuth)
+                .setTimeout(60L, TimeUnit.SECONDS)
+                .setPhoneNumber(phoneNumber)
+                .setActivity(requireActivity())
+                .setCallbacks(phoneAuthVerificationStateChangeCallbacks)
+                .build();
+        PhoneAuthProvider.verifyPhoneNumber(authOptions);
+    }
+
+    private void setVisibility(int progressBar, int cl, int pinView) {
+        binding.progressBar.setVisibility(progressBar);
+        binding.phoneLoginCLayout.setVisibility(cl);
+        binding.firstPinView.setVisibility(pinView);
     }
 
     @Override
