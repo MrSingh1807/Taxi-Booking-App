8bc57caeaf9a933f579fe503be6c2145685c7a68
Mayank Singh Shakya <mayank1807.singh@gmail.com>
2023-04-06 00:14:00 +0530

    add auto selecter api

--- app/src/main/java/com/example/taxibookingapp/UI/AuthenticateUser/PhoneLoginFragment.java
+++ app/src/main/java/com/example/taxibookingapp/UI/AuthenticateUser/PhoneLoginFragment.java
@@ -1,22 +1,30 @@
 package com.example.taxibookingapp.UI.AuthenticateUser;
 
-import android.app.AlertDialog;
-import android.graphics.drawable.Animatable;
+import static android.app.Activity.RESULT_OK;
+
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
 import android.widget.Toast;
 
-import com.example.taxibookingapp.R;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
 import com.example.taxibookingapp.databinding.FragmentPhoneLoginBinding;
+import com.google.android.gms.auth.api.credentials.Credential;
+import com.google.android.gms.auth.api.credentials.CredentialsApi;
+import com.google.android.gms.auth.api.identity.GetPhoneNumberHintIntentRequest;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.common.api.ApiException;
 import com.hbb20.CountryCodePicker;
 
 
@@ -24,6 +32,34 @@ public class PhoneLoginFragment extends Fragment {
 
     FragmentPhoneLoginBinding binding;
     private String SELECTED_COUNTRY_CODE = "+91";
+    private static final int CREDENTIAL_PICKER_REQUEST = 120;
+    public static final String TAG = "MeriTaxi";
+    ActivityResultLauncher<IntentSenderRequest> phoneNumberHintIntentResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartIntentSenderForResult(), new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result != null && result.getData() != null) {
+                        if (result.getResultCode() == RESULT_OK) {
+                            String phoneNumber;
+                            try {
+                                phoneNumber = Identity.getSignInClient(requireActivity()).getPhoneNumberFromIntent(result.getData());
+                                if (phoneNumber.contains("+91")){
+                                    phoneNumber = phoneNumber.replace("+91", "");
+                                }
+                            } catch (ApiException e) {
+                                throw new RuntimeException(e);
+                            }
+                            Toast.makeText(requireContext(), "MOB " + phoneNumber, Toast.LENGTH_SHORT).show();
+                            binding.contactNumberET.setText(phoneNumber);
+
+                        } else {
+                            Toast.makeText(requireContext(), "No phone numbers found", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                }
+            });
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -31,7 +67,7 @@ public class PhoneLoginFragment extends Fragment {
         binding = FragmentPhoneLoginBinding.inflate(inflater, container, false);
         // Inflate the layout for this fragment
 
-        //   Country Code Picker
+        /* ************  Country Code Picker  ************** */
         binding.countryCodePicker.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
             @Override
             public void onCountrySelected() {
@@ -39,7 +75,7 @@ public class PhoneLoginFragment extends Fragment {
             }
         });
 
-        // EditText
+        /* ************  EditText  ************** */
         binding.contactNumberET.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
@@ -51,16 +87,18 @@ public class PhoneLoginFragment extends Fragment {
             public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
                 if (charSequence.length() == 10) {
                     Toast.makeText(requireContext(), "OTP is sent to your Mobile Number ", Toast.LENGTH_SHORT).show();
+                    /*
                     binding.phoneLoginCLayout.animate()
                             .translationY(binding.phoneLoginCLayout.getHeight())
                             .alpha(0.0f)
                             .setDuration(500);
-//                    binding.phoneLoginCLayout.setVisibility(View.GONE);
+                    binding.phoneLoginCLayout.setVisibility(View.GONE);
                     binding.firstPinView.animate()
                             .translationY(binding.firstPinView.getHeight())
                             .alpha(1.0f)
                             .setDuration(500);
                     binding.firstPinView.setVisibility(View.VISIBLE);
+                     */
                 }
             }
 
@@ -70,7 +108,7 @@ public class PhoneLoginFragment extends Fragment {
             }
         });
 
-        // Pin View
+        /* ************  Pin View  ************** */
         binding.firstPinView.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
@@ -89,6 +127,34 @@ public class PhoneLoginFragment extends Fragment {
 
             }
         });
+
+        /* ************   Auto Phone Select  ************** */
+        /*  HintRequest hintRequest = new HintRequest.Builder()
+                .setPhoneNumberIdentifierSupported(true)
+                .build();
+
+
+        PendingIntent intent = Credentials.getClient(requireContext()).getHintPickerIntent(hintRequest);
+        try
+        {
+            startIntentSenderForResult(intent.getIntentSender(), CREDENTIAL_PICKER_REQUEST, null, 0, 0, 0,new Bundle());
+        }
+        catch (IntentSender.SendIntentException e)
+        {
+            e.printStackTrace();
+        }
+         */
+        GetPhoneNumberHintIntentRequest hintRequest = GetPhoneNumberHintIntentRequest.builder().build();
+        Identity.getSignInClient(requireActivity())
+                .getPhoneNumberHintIntent(hintRequest)
+                .addOnSuccessListener(pendingIntent -> {
+                    phoneNumberHintIntentResultLauncher.launch(
+                            new IntentSenderRequest.Builder(pendingIntent.getIntentSender()).build()
+                    );
+                })
+                .addOnFailureListener(e -> {
+                    Log.d(TAG, e.getMessage());
+                });
 
 
         return binding.getRoot();
